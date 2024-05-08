ed82ddec86b10759909df167fef885909736dd19
psgexam <151575056+psgexam@users.noreply.github.com>
2024-05-09 00:24:05 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+
+sudo dpkg -i libgl1-mesa-glx_23.0.4-0ubuntu1.22.04.1_amd64.deb libegl1-mesa_23.0.4-0ubuntu1.22.04.1_amd64.deb
+
+sudo apt-get install libxcb-xinerama0-dev
+
+#sudo dpkg -i Packet_Tracer822_amd64_signed.deb
